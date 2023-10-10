c9ef6880df4755f1b5bb6a71b309f9fe5a0f3c6c
Rajnikumar Tank <125719786+Tank-Rajnikumar@users.noreply.github.com>
2023-10-10 15:38:44 +0530

    Time: 58 ms (51.92%), Space: 16.3 MB (17.43%) - LeetHub

--- 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.py
+++ 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.py
@@ -1,15 +1,17 @@
 class Solution:
     def sortEvenOdd(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            for odd in range(1,len(nums)-2,2):
-                if nums[odd] < nums[odd+2]:
-                    temp=nums[odd]
-                    nums[odd]=nums[odd+2]
-                    nums[odd+2]=temp
-        for i in range(len(nums)):
-            for even in range(0,len(nums)-2,2):
-                if nums[even] > nums[even+2]:
-                    temp=nums[even]
-                    nums[even]=nums[even+2]
-                    nums[even+2]=temp
+        # for i in range(len(nums)//2):
+        #     for odd in range(1,len(nums)-2,2):
+        #         if nums[odd] < nums[odd+2]:
+        #             temp=nums[odd]
+        #             nums[odd]=nums[odd+2]
+        #             nums[odd+2]=temp
+        # for i in range(len(nums)//2):
+        #     for even in range(0,len(nums)-2,2):
+        #         if nums[even] > nums[even+2]:
+        #             temp=nums[even]
+        #             nums[even]=nums[even+2]
+        #             nums[even+2]=temp
+        # return nums
+        nums[::2],nums[1::2]=sorted(nums[::2]),sorted(nums[1::2],reverse=True)
         return nums
